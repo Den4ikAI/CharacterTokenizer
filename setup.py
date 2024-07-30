4816e4c893bd28775133c8661915b9b4a921cc9a
Denis <61190545+Den4ikAI@users.noreply.github.com>
2024-07-31 04:15:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     version="1.0.0",
     author="Denis Petrov",
     author_email="arduino4b@gmail.com",
-    description="Хуйня паскудная",
+    description="Хуйня паскудная мне весь мозг выебала",
     url="https://github.com/Den4ikAI/CharacterTokenizer",
     packages=setuptools.find_packages(),
     include_package_data=False,
