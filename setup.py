d2abde2c6d95b15e4ee7ca51519b5a276c624458
Denis <61190545+Den4ikAI@users.noreply.github.com>
2024-07-31 04:12:59 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import io
+import setuptools
+
+setuptools.setup(
+    name="chartk",
+    version="1.0.0",
+    author="Denis Petrov",
+    author_email="arduino4b@gmail.com",
+    description="Хуйня паскудная",
+    url="https://github.com/Den4ikAI/CharacterTokenizer",
+    packages=setuptools.find_packages(),
+    include_package_data=False,
+)
